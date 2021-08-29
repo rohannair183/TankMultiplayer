e132e8103ac7a20754de041f54c752a1553c8777
rohannair183 <Rohannair183@gmail.com>
2021-08-29 13:59:51 -0600

    client server change

--- Public/sketch.js
+++ Public/sketch.js
@@ -34,7 +34,7 @@ const TERRIAN_BLOCK_SIZE = 15;
 function preload() {
 console.log(localStorage.getItem('tankUsername'));
   //Connect client to server
-  socket = io.connect("https://tankgamerohan.herokuapp.com/");
+  socket = io.connect(process.env.PORT);
   socket.on("connect", () => {
     id = socket.id;
     console.log(id);
